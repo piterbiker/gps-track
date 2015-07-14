4d77527f4e41e834e985554435ec6807756d29f3
Piotr <python@dolinka.katowice.pl>
2015-07-14 21:58:46 +0200

    2015-07-14_II

---
+++ katalogi.py
@@ -0,0 +1,9 @@
+# ------------------------------
+# config file with path to file
+# ------------------------------
+
+# main path to data file
+kat = 'D:\\'
+# output path to data file
+podkat = 'wpt\\'
+
